d430c8652c36ab299dfa9d41450e3f7f0ef4416f
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-12-22 00:27:04 +0100

    word equality features renamed + NE features

--- lib/Treex/Tool/Coreference/Features/RU/Noun.pm
+++ lib/Treex/Tool/Coreference/Features/RU/Noun.pm
@@ -18,7 +18,8 @@ augment '_unary_features' => sub {
     my ($self, $node, $type) = @_;
     
     my $feats = {};
-    $self->cs_np_unary_feats($feats, $node, $type);
+    $self->wordeq_unary_feats($feats, $node, $type);
+    $self->ne_unary_feats($feats, $node, $type);
 
     my $sub_feats = inner() || {};
     return { %$feats, %$sub_feats };
@@ -28,24 +29,51 @@ override '_binary_features' => sub {
     my ($self, $set_feats, $anaph, $cand, $candord) = @_;
     my $feats = super();
     
-    $self->cs_np_binary_feats($feats, $set_feats, $anaph, $cand, $candord);
+    $self->wordeq_binary_feats($feats, $set_feats, $anaph, $cand, $candord);
+    $self->ne_binary_feats($feats, $set_feats, $anaph, $cand, $candord);
     return $feats;
 };
 
 
-################## MORPHO-(DEEP)SYNTAX FEATURES ####################################
+################## WORD EQUALITY AND SIMILARITY FEATURES ####################################
 
-sub cs_np_unary_feats {
+sub wordeq_unary_feats {
     my ($self, $feats, $node, $type) = @_;
-    $feats->{full_np_set} = join " ", (uniq sort map {$_->t_lemma} $node->get_descendants());
+    $feats->{full_np_set} = join "_", (uniq sort map {$_->t_lemma} $node->get_descendants({add_self => 1, ordered => 1}));
 }
 
-sub cs_np_binary_feats {
+sub wordeq_binary_feats {
     my ($self, $feats, $set_feats, $anaph, $cand, $candord) = @_;
-    $feats->{head_eq} = $set_feats->{"c^cand_lemma"} eq $set_feats->{"a^anaph_lemma"} ? 1 : 0;
-    $feats->{head_dist} = distance($set_feats->{"c^cand_lemma"}, $set_feats->{"a^anaph_lemma"});
-    $feats->{full_np_set_eq} = $set_feats->{"c^cand_full_np_set"} eq $set_feats->{"a^anaph_full_np_set"} ? 1 : 0;
-    $feats->{full_np_set_dist} = distance($set_feats->{"c^cand_full_np_set"}, $set_feats->{"a^anaph_full_np_set"});
+    $feats->{agree_lemma} = $self->_agree_feats($set_feats->{"c^cand_lemma"}, $set_feats->{"a^anaph_lemma"});
+    $feats->{join_lemma} = $self->_join_feats($set_feats->{"c^cand_lemma"}, $set_feats->{"a^anaph_lemma"});
+    $feats->{dist_lemma} = distance($set_feats->{"c^cand_lemma"}, $set_feats->{"a^anaph_lemma"});
+    
+    $feats->{agree_full_np_set} = $self->_agree_feats($set_feats->{"c^cand_full_np_set"}, $set_feats->{"a^anaph_full_np_set"});
+    $feats->{join_full_np_set} = $self->_join_feats($set_feats->{"c^cand_full_np_set"}, $set_feats->{"a^anaph_full_np_set"});
+    $feats->{dist_full_np_set} = distance($set_feats->{"c^cand_full_np_set"}, $set_feats->{"a^anaph_full_np_set"});
+}
+
+######################### NAMED ENTITY FEATURES ###############################################
+
+sub ne_unary_feats {
+    my ($self, $feats, $node, $type) = @_;
+    my $nnode = $node->get_n_node;
+    $feats->{is_ne} = defined $nnode ? $b_true : $b_false;
+    return if (!defined $nnode);
+    $feats->{ne_type} = $nnode->ne_type;
+    $feats->{ne_normname} = $nnode->normalized_name;
+}
+
+sub ne_binary_feats {
+    my ($self, $feats, $set_feats, $anaph, $cand, $candord) = @_;
+    $feats->{agree_is_ne} = $self->_agree_feats($set_feats->{"c^cand_is_ne"}, $set_feats->{"a^anaph_is_ne"});
+    $feats->{join_is_ne} = $self->_join_feats($set_feats->{"c^cand_is_ne"}, $set_feats->{"a^anaph_is_ne"});
+    $feats->{agree_ne_type} = $self->_agree_feats($set_feats->{"c^cand_ne_type"}, $set_feats->{"a^anaph_ne_type"});
+    $feats->{join_ne_type} = $self->_join_feats($set_feats->{"c^cand_ne_type"}, $set_feats->{"a^anaph_ne_type"});
+    $feats->{agree_ne_normname} = $self->_agree_feats($set_feats->{"c^cand_ne_normname"}, $set_feats->{"a^anaph_ne_normname"});
+    $feats->{join_ne_normname} = $self->_join_feats($set_feats->{"c^cand_ne_normname"}, $set_feats->{"a^anaph_ne_normname"});
+    return if (!defined $set_feats->{"c^cand_ne_normname"} || !defined $set_feats->{"a^anaph_ne_normname"});
+    $feats->{dist_ne_normname} = distance($set_feats->{"c^cand_ne_normname"}, $set_feats->{"a^anaph_ne_normname"});
 }
 
 1;
