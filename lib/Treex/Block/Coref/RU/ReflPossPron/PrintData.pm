610a0388f93b7b853c77d3c2a7793f7b6dda655e
Michal Novak <mnovak@ufal.mff.cuni.cz>
2017-01-21 14:14:05 +0100

    blocks for russian svoj

---
+++ lib/Treex/Block/Coref/RU/ReflPossPron/PrintData.pm
@@ -0,0 +1,8 @@
+package Treex::Block::Coref::RU::ReflPossPron::PrintData;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Coref::PrintData';
+with 'Treex::Block::Coref::RU::ReflPossPron::Base';
+
+1;
+#TODO add documentation
---
+++ lib/Treex/Block/Coref/RU/ReflPossPron/Resolve.pm
@@ -0,0 +1,61 @@
+package Treex::Block::Coref::RU::ReflPossPron::Resolve;
+use Moose;
+use Treex::Core::Common;
+extends 'Treex::Block::Coref::Resolve';
+with 'Treex::Block::Coref::RU::ReflPossPron::Base';
+
+#use Treex::Tool::Coreference::PerceptronRanker;
+#use Treex::Tool::Coreference::RuleBasedRanker;
+#use Treex::Tool::Coreference::ProbDistrRanker;
+use Treex::Tool::ML::VowpalWabbit::Ranker;
+
+override 'build_model_path' => sub {
+    # no rulebased CR for EN -> almost no relpron coreference
+    #default => '/home/mnovak/projects/coref_projection/treex_cr_train/ru/relpron/tmp/ml/001_run_2016-12-28_00-19-46_27180.russian_relprons/001.8ba2e.mlmethod/model/train.official.table.gz.vw.ranking.model',
+    # rulebased CR for EN
+    my $path = '/home/mnovak/projects/coref_projection/treex_cr_train/ru/relpron/tmp/ml/002_run_2017-01-06_18-22-08_32556.projected_EN_rulebased_relprons/003.ad7ca.mlmethod/model/train.official.table.gz.vw.ranking.model';
+    print STDERR "MODEL_PATH: $path\n";
+    return $path;
+};
+
+override '_build_ranker' => sub {
+    my ($self) = @_;
+#    my $ranker = Treex::Tool::Coreference::RuleBasedRanker->new();
+#    my $ranker = Treex::Tool::Coreference::ProbDistrRanker->new(
+#    my $ranker = Treex::Tool::Coreference::PerceptronRanker->new( 
+    my $ranker = Treex::Tool::ML::VowpalWabbit::Ranker->new( 
+        { model_path => $self->model_path } 
+    );
+    return $ranker;
+};
+
+1;
+
+#TODO adjust documentation
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME 
+
+Treex::Block::Coref::RU::ReflPossPron::Resolve
+
+=head1 RUSCRIPTION
+
+Pronoun coreference resolver for German.
+Settings:
+* German personal pronoun filtering of anaphor
+* candidates for the antecedent are nouns from current (prior to anaphor) and previous sentence
+* German pronoun coreference feature extractor
+* using a model trained by a VW ranker
+
+=head1 AUTHORS
+
+Michal Novák <mnovak@ufal.mff.cuni.cz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2011-2016 by Institute of Formal and Applied Linguistics, Charles University in Prague
+
+This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
