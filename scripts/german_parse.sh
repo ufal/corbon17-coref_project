9571e998f390a66a4908f17914bbadc4d0ffab76
Michal Novak <mnovak@ufal.mff.cuni.cz>
2017-02-09 18:09:17 +0100

    bugifx?

--- scripts/german_parse.sh
+++ scripts/german_parse.sh
@@ -13,7 +13,7 @@ if [ $is_conll == 0 ]; then
     cat $infile | cut -f2 > $tmpdir/de.words.txt
     java -Xmx2G -classpath tools/transition-1.30.jar is2.util.Split $tmpdir/de.words.txt > $tmpdir/de.words.conll
 else
-    cat $infile | grep -v '^#' | perl -ne 'chomp $_; my ($ord, $word, @rest) = split /\t/, $_; for (my $i=0; $i<@rest; $i++) { $rest[$i] =~ s/-/_/g; } print join "\t", ($ord, $word, @rest); print "\n";' > $tmpdir/de.words.conll
+    cat $infile | grep -v '^#' | perl -ne 'chomp $_; my ($ord, $word, @rest) = split /\t/, $_; for (my $i=0; $i<@rest; $i++) { $rest[$i] =~ s/-/_/g; }; $rest[12] = "_"; print join "\t", ($ord, $word, @rest); print "\n";' > $tmpdir/de.words.conll
 fi
 java -Xmx2G -classpath tools/transition-1.30.jar is2.lemmatizer2.Lemmatizer -test $tmpdir/de.words.conll -out $tmpdir/de.words.lemmas.conll -model tools/models/lemma-ger-3.6.model
 java -Xmx40G -classpath tools/transition-1.30.jar is2.transitionS2a.Parser -test $tmpdir/de.words.lemmas.conll -out $tmpdir/de.parse.conll -model tools/pet-ger-S2a-40-0.25-0.1-2-2-ht4-hm4-kk0
